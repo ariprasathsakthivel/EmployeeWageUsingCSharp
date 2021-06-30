88d75fb88b410d64080c9137b0b0a1ce225b0433
ariprasathsakthivel <ariprasath3@gmail.com>
2021-06-30 10:03:48 +0530

    added:upto UC4

---
+++ EmployeeUC6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EmployeeUC6
+{
+    class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
+        static void Main(string[] args)
+        {
+            //Variable
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            //Computation
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+            }
+            int totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalEmpwage);
+        }
+    }
+}
